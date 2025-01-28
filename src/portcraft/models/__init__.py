ebf8d6a087c7beabec00f40c7536a4a411bb7612
vortexdude <nitinnamdeo456@gmail.com>
2025-01-28 22:30:34 +0530

    :hammer: fix installation

--- src/portcraft/models/__init__.py
+++ src/portcraft/models/__init__.py
@@ -1,72 +1,63 @@
-class Model:
-    def __init__(self, attrs=None):
-        self.attrs = attrs
+import os.path
 
-    def _getter(self, attr_name: str):
-        if attr_name in self.attrs:
-            return self.attrs.get(attr_name)
-        return None
-
-    @classmethod
-    def prepare_model(cls, *args, **kwargs):
-        return cls(*args, **kwargs)
-
-    def __call__(self):
-        return f"<{self.__class__.__name__} {self.name}>"
-
-    def __str__(self):
-        return f"<{self.__class__.__name__} {self.name}>"
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__} {self.name}>"
+from portcraft.lib.common import find_module_file
+from portcraft.settings import pu
 
 
-class Stage(Model):
-    @property
-    def name(self):
-        return self._getter('name')
+class Module:
+    def __init__(self, attrs):
+        self._attrs = attrs
+        self._name: str | None = None
+        self._comment = None
+        self._module_args = {}
+        self._plugins = {}
+        self.format()
+        if not self._name:
+            raise ValueError(f"No module defined in the config.")
 
-    @property
-    def args(self):
-        return self._getter('args')
+        self._module_file = find_module_file(self._name, pu.library_paths)
+        if not self._module_file:
+            raise FileNotFoundError(f"Module not found '{self._name}'")
 
-    @property
-    def modules(self):
-        return [Module.prepare_model(module) for module in self.args]
+    @staticmethod
+    def checker(key):
+        for item in pu.library_paths:
+            lib_file_slug = os.path.join(item, key)
+            if os.path.exists(lib_file_slug):
+                print(f"its {key} a module")
+        for item in pu.plugins_path:
+            plugin_file_slug = os.path.join(item, key)
+            if os.path.exists(plugin_file_slug):
+                print(f"its {key} a lib")
 
 
-class Module(Model):
-    def __init__(self, attr):
-        super().__init__(attr)
-        self._name, self._args = self.argument_extractor()
-
-    def argument_extractor(self):
-        if not self.attrs:
-            raise ValueError("The 'attrs' dictionary is empty. Cannot extract arguments.")
-
-        extracted_name = (
-            next((key for key in self.attrs if key != 'name'), None)
-            if 'name' in self.attrs
-            else next(iter(self.attrs), None)
-        )
-        return extracted_name, self.attrs.get(extracted_name)
-
+    def format(self):
+        for key, value in self._attrs.items():
+            self.checker(key)
+            if key == "name":  # grab the comment if possible
+                self._comment = value
+            elif isinstance(value, dict):  # check for the args for the module
+                self._name = key
+                self._module_args = value
+            else:
+                self._plugins[key] = value
 
     @property
-    def metadata(self) -> str | None:
-        if 'name' in self.attrs:
-            return self.attrs.get('name', None)
+    def comment(self) -> str:
+        return self._comment
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self._name
 
     @property
-    def arguments(self):
-        return self._args
+    def args(self):
+        return self._module_args
 
     @property
-    def register(self):
-        if 'register' in self.attrs:
-            return self.attrs.get('register')
-        return None
+    def plugins(self) -> dict:
+        return self._plugins
+
+    @property
+    def file(self) -> str:
+        return self._module_file
